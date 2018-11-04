c8584588061e7f6d326296c70c340d1130e1c7e6
Gieralt, Artur (Nokia - PLWroclaw) <artur.gieralt@nokia.com>
2018-11-04 01:09:50 +0100

    Highlights

--- src/components/Task/Task.tsx
+++ src/components/Task/Task.tsx
@@ -1,14 +1,24 @@
 import * as React from "react";
 import { ITaskProps } from './Task.types';
 import './Task.css';
+import * as moment from 'moment';
 
 export const Task = (props: ITaskProps) => {
     const { task, onClick } = props;
+    const style: React.CSSProperties = {
+        backgroundColor: 'yellow'
+    }
     const changeTaskStatus = () => onClick(task.id, !task.isDone);
+    let is24HoursBeforeDeadline = false;
+    if (task.expiryDate) {
+        const diff = task.expiryDate.diff(moment(), "h");
+        is24HoursBeforeDeadline = (diff > 0 && diff < 24 )
+    }
+    
     return (
         <div className='taskContainer'>
             <input type='checkbox' onChange={changeTaskStatus} checked={task.isDone}/>
-            <h2>{task.title}</h2>
+            <h2 style={is24HoursBeforeDeadline ? style : {}}>{task.title}</h2>
             <span>{task.expiryDate && task.expiryDate.toLocaleString()}</span>
         </div>
     );
